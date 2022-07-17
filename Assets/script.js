871073bfaa24a8d95edf8d4d6dde75318701c111
PsychoRSS <Bobgonzalez516@gmail.com>
2022-07-16 23:34:49 -0600

    added working timer

--- Assets/script.js
+++ Assets/script.js
@@ -3,28 +3,19 @@ var button2 = document.getElementById("button-2");
 var button3 = document.getElementById("button-3");
 var button4 = document.getElementById("button-4");
 var start = document.getElementById("start")
-var timer = document.getElementById("timer")
-
-var secondsleft = 240;
 
 
-var timmer =setInterval(() => {
-    var now = new Date().getTime();
-    var timeleft = countDownDate - now;
-    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-}, 1000);
+const startingMinutes = 3;
+let time = startingMinutes * 60;
 
-// function timer() {
-//     var timerinterval = setInterval(function() {
-//         secondsleft --;
-//         timerinterval.textcontent = secondsleft + " seconds left till quiz is over"
+setInterval(updateCountdown, 1000);
+const countdownEL = document.getElementById('timer')
+function updateCountdown(){
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
 
-//         if(secondsleft === 0) {
-//             clearInterval(timerinterval);
-//             sendMessage();
-//         }
-//     }, 1000);
-// }
-// var questions = ['What is a variable?', 'What is a Boolien?' ]
-console.log(timer);
- 
+    countdownEL.innerHTML = `${minutes}: ${seconds}`;
+    time--;
+}
+
+console.log(timmer);
