4b922df27038d5544794644022ef6253bd2f80fd
PsychoRSS <Bobgonzalez516@gmail.com>
2022-07-23 08:11:36 -0600

    added a way to connect it to a button sorta

--- Assets/script.js
+++ Assets/script.js
@@ -2,15 +2,23 @@ var button1 = document.getElementById("button-1");
 var button2 = document.getElementById("button-2");
 var button3 = document.getElementById("button-3");
 var button4 = document.getElementById("button-4");
-var start = document.getElementById("start")
+
+
+// start button
+function startbttn(){
+    var start = document.getElementById("start")
+    document.addEventListener("click",start).updateCountdown()
+}
 
 // timer
 const startingMinutes = 3;
 let time = startingMinutes * 60;
 
+
 setInterval(updateCountdown, 1000);
 const countdownEL = document.getElementById('timer')
 function updateCountdown(){
+
     const minutes = Math.floor(time / 60);
     let seconds = time % 60;
 
@@ -23,6 +31,9 @@ function updateCountdown(){
         document.getElementById("timer").innerHTML = "expired"
     }
 }
+
+
+
 
 // questions
 var questions = ['What is a variable?', 'What is a Boolien?' ]
